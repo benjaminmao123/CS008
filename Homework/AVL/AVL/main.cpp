8c0a44d7c1031901a256356649edc357bfbb8ed6
benjaminmao123 <43651347+benjaminmao123@users.noreply.github.com>
2020-09-24 22:52:31 -0700

    Update main.cpp

--- Homework/AVL/AVL/main.cpp
+++ Homework/AVL/AVL/main.cpp
@@ -4,16 +4,84 @@
 #include <cctype>
 #include <random>
 
+void Test();
+
 int main()
 {
-	AVL<int> avl;
-
-	for (int i = 1; i <= 5; ++i)
-	{
-		std::cout << "Iteration: " << i << std::endl;
-		avl.insert(i);
-		std::cout << avl << std::endl;
-	}
+	Test();
 
 	return 0;
+}
+
+void Test()
+{
+	char input = '\0';
+	AVL<int> avl;
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dist(0, 1000);
+
+	while (input != 'x')
+	{
+		std::cout << "[R]andom [I]nsert [C]lear [S]earch [E]rase E[x]it: ";
+		std::cin >> input;
+
+		switch (tolower(input))
+		{
+		case 'r':
+		{
+			int item = dist(gen);
+			std::cout << "Insert: " << item << std::endl;
+
+			avl.insert(item);
+			break;
+		}
+		case 'i':
+		{
+			int item;
+			std::cout << "Insert: ";
+			std::cin >> item;
+			std::cout << std::endl;
+
+			avl.insert(item);
+			break;
+		}
+		case 'c':
+		{
+			AVL<int> temp;
+			avl.swap(temp);
+			break;
+		}
+		case 's':
+		{
+			tree_node<int>* fptr;
+			int item;
+			std::cout << "Search: ";
+			std::cin >> item;
+			std::cout << std::endl;
+
+			if (avl.search(item, fptr))
+				std::cout << "Item is found." << std::endl;
+			else
+				std::cout << "Item is not found." << std::endl;
+			break;
+		}
+		case 'e':
+		{
+			int item;
+			std::cout << "Erase: ";
+			std::cin >> item;
+			std::cout << std::endl;
+
+			avl.erase(item);
+			break;
+		}
+		case 'x':
+			return;
+		default:
+			break;
+		}
+
+		std::cout << avl << std::endl;
+	}
 }
